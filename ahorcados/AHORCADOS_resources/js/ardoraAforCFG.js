4c95f13b1bcccb76f38a9936df72c66f23ba7da2
jeferson-macias <jeferson0216@hotmail.com>
2016-10-27 15:26:26 -0500

    entrega
    entrega

---
+++ ahorcados/AHORCADOS_resources/js/ardoraAforCFG.js
@@ -0,0 +1,32 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=20;
+var successes=0; successesMax=1; attempts=0; attemptsMax=3;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=0
+var typeGame=0;
+var tiTime=true;
+var tiTimeType=2;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=true;
+var tiAttempts=true;
+var tiScore=true;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#9F1114"; colorText="#000000"; colorSele="#6C7B75";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Felicidades, buen trabajo"; messageTime="Fallaste por tiempo"; messageError=""; messageErrorG=""; messageAttempts="Fallaste por intentos"; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="QUhPUkNBRE9T"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["Q09ESUdP","","","","","","","","",""];imaW=["vamos.jpg","","","","","","","","",""];queW=["","","","","","","","","",""];
+var auW=["","","","","","","","","",""];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ "; wordsG=[]; indexG=0; actMaxWidth=550; indexWord=0; totalWord=0;
+var imageW=[];questionW=[];audioW=[];profG=0; dirMedia="AHORCADOS_resources/media/"; textBNext="";
